8ee1d5ab5a918542e73d79a2a49b902fa99fc9b7
Patrick Prakash <patrickprakash8@gmail.com>
2018-08-14 22:51:30 +0530

    Minor Changes

--- app/src/main/java/com/mh/treasurehuntmh/HintImageActivity.java
+++ app/src/main/java/com/mh/treasurehuntmh/HintImageActivity.java
@@ -3,11 +3,12 @@ package com.mh.treasurehuntmh;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class HintActivity extends AppCompatActivity {
+public class HintImageActivity extends AppCompatActivity {
 
+    private static final String TAG = "HintImageActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hint);
+        setContentView(R.layout.activity_hint_image);
     }
 }
